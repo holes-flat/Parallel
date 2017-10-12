f29baa52a8bf20ee9cf179c4b13dd3c30305954e
holes-flat <shaoyuanmcm1@163.com>
2017-10-12 10:27:22 +0800

    create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/******************
+>File Name:main.c
+>Author:Yang Yichen
+>Create Time:2017年10月12日 星期四 10时24分38秒
+>Brief:
+*********************/
+
+#include <stdio.h>
+int main(void){
+    printf("a begining");
+    return 0;
+}
+
